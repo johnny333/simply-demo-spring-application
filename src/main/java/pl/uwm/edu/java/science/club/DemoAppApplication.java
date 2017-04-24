b7b752b5b75043dc37ce07b5e7a0efabec7ca016
Jakub Kołecki <jakub.kolecki@programisci.eu>
2017-04-24 23:11:34 +0200

    first commit to start coding tutorial

---
+++ src/main/java/pl/uwm/edu/java/science/club/DemoAppApplication.java
@@ -0,0 +1,12 @@
+package pl.uwm.edu.java.science.club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppApplication.class, args);
+	}
+}
